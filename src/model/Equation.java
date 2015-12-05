886a03ebfb51bbf2451bd9a98886129aabf921bb
Aakov <Aakov@aakov-dy>
2015-12-06 01:04:03 +0800

    Updated Equation to create matrix

--- src/model/Equation.java
+++ src/model/Equation.java
@@ -9,6 +9,7 @@ public class Equation {
 	private String rightEquation;
 	private ArrayList<Compound> compounds = new ArrayList<Compound>();	// Get chemical compounds
 	private ArrayList<Element> elements = new ArrayList<Element>(); // Get all elements from the equation
+	private double[][] matrix;
 	
 	/* Note: Right equation has negative coefficients */
 	public Equation(String leftEquation, String rightEquation){
@@ -16,6 +17,8 @@ public class Equation {
 		this.rightEquation = rightEquation;
 		storeCompounds(leftEquation, "LEFT");
 		storeCompounds(rightEquation, "RIGHT");
+		getAllElements();
+		toMatrix();
 	}
 	
 	public void storeCompounds(String equation, String side){
@@ -53,7 +56,67 @@ public class Equation {
 	
 	/* Get all elements in the equation */
 	public void getAllElements(){
+		boolean unique = false;
+		/* Check all compounds for elements */
+		for (int i = 0; i < compounds.size(); i++){
+			/* Check each elements in a compound */
+			for (int j = 0; j < compounds.get(i).getCompoundElements().size(); j++){
+				if (elements.size() == 0){
+					elements.add(compounds.get(i).getCompoundElements().get(j));
+				} else{
+					unique = true;
+					for (int k = 0; k < elements.size(); k++){
+						if (compounds.get(i).getCompoundElements().get(j).isEqual(elements.get(k).getElementAbbrev())){
+							unique = false;
+						}
+					}
+					if(unique == true){
+						elements.add(compounds.get(i).getCompoundElements().get(j));
+					}
+				}
+			}
+		}
+	}
+	
+	/* Make a matrix of n x n+1 */
+	public void toMatrix(){
+		matrix = new double[elements.size()][compounds.size() + 1];
+		boolean exist = false;
+		int index = 0;
 		
+		// Column
+		for (int i = 0; i < compounds.size() + 1; i++){
+			
+			if (i == compounds.size()){
+				for (int j = 0; j < elements.size(); j++){
+					matrix[j][i] = 0;
+				}
+			} else {
+				//Row
+				for (int j = 0; j < elements.size(); j++){
+					for (int k = 0; k < compounds.get(i).getCompoundElements().size(); k++){
+						if (compounds.get(i).getCompoundElements().get(k).isEqual(elements.get(j).getElementAbbrev())){
+							exist = true;
+							index = k;
+						}
+					}
+					if (exist){
+						matrix[j][i] = compounds.get(i).getCompoundElements().get(index).getCoefficient();
+						exist = false;
+					} else {
+						matrix[j][i] = 0;
+					}
+				}
+			}
+		}
+		
+		for (int i = 0; i < elements.size(); i++){
+			System.out.print("|");
+			for(int j = 0; j < compounds.size() + 1; j++){
+				System.out.print(matrix[i][j] + " ");
+			}
+			System.out.println("|");
+		}
 	}
 	
 }
